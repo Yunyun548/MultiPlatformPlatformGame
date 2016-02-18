d71430838af8a8e6cdeef1d8f648bad7b5f7de9b
LudovicP <ludovic.panchaud@ynov.com>
2016-02-18 18:22:16 +0100

    Init new projects

---
+++ MultiplatformPlatformGame.Gameplay/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplatformPlatformGame.Gameplay
+{
+    public class Class1
+    {
+    }
+}
