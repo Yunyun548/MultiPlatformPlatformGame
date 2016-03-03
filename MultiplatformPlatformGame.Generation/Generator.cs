0a115e2606927a82ab4219d74ac30bf257a4aac0
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2016-03-03 14:45:22 +0100

    Generator optimisation.

--- MultiplatformPlatformGame.Generation/Generator.cs
+++ MultiplatformPlatformGame.Generation/Generator.cs
@@ -5,195 +5,198 @@ using System.Linq;
 
 namespace MultiplatformPlatformGame.Generation
 {
-	public class Generator
-	{
-        // Constantes pour la configuration (déplacer dans configuration)
-        private const int ChunkSize = 100;
-        private const int JoinChance = 30;
-        private const int VerticalChance = 25;
-        private const int HorizontalChance = 75;
-
+    public class Generator
+    {
         private readonly Random _r;
 
-		private List<Component> components;
-		private List<Block> blocks;
+        private List<Component> components;
+        private List<Block> blocks;
 
-		public Generator ()
-		{
+        public Generator()
+        {
             this._r = new Random(DateTime.Now.Millisecond);
-			components = new List<Component> ();
-			blocks = new List<Block> ();
-		}
-
-		public void AddComponent(String filePath)
-		{
-			Component c = JsonConvert.DeserializeObject<Component> (System.IO.File.ReadAllText(filePath));
-			components.Add (c);
-		}
-
-		public void AddBlock(String filePath)
-		{
-			Block b = JsonConvert.DeserializeObject<Block> (System.IO.File.ReadAllText(filePath));
-			blocks.Add (b);
-		}
-
-		public List<Block> GetBlocks()
-		{
-			return blocks;
-		}
-
-		public void AnalyseBlocks()
-		{
-			foreach (Block b in blocks) {
-				b.BindComponents (components);
-				AnalyseBlock (b);
-			}
-		}
-
-		protected void AnalyseBlock(Block b)
-		{
-			foreach (BlockComponent c in BorderNonSolidComponents(b)) {
-				
-			}
-		}
-
-		protected IEnumerable<BlockComponent> BorderComponents(Block b)
-		{
-			for (int i = 0; i < b.BlockSize; i++) {
-				if (i == 0 || i == b.BlockSize) {
-					foreach (BlockComponent c in b.ComponentMatrix[i]) {
-						yield return c;
-					}
-				} else {
-					yield return b.ComponentMatrix [i] [0];
-					yield return b.ComponentMatrix [i] [b.BlockSize - 1];
-				}
-			}
-		}
-
-		protected IEnumerable<BlockComponent> BorderNonSolidComponents(Block b)
-		{
-			foreach (BlockComponent c in BorderComponents(b)) {
-				if (!c.Component.Solid) {
-					yield return c;
-				}
-			}
-		}
-
-        public void GenerateChunck()
-        {
-            List<Room> rooms = new List<Room>();
-
-            // Point de départ
-            Point start = new Point(0, 0);
-            Room startRoom = new Room
-            {
-                Position = start,
-                OpenWalls = Opening.None
-            };
-            rooms.Add(startRoom);
-
-
-            // Génération des autres pièces
-            int cmpt = 0;
-            while (rooms.Count < Generator.ChunkSize)
-            {
-                Room currentRoom;
-                if (cmpt >= rooms.Count)
-                {
-                    int topRight = rooms.Max(ro => ro.Position.X);
-                    currentRoom = rooms.First(ro => ro.Position.X == topRight);
-                    currentRoom.OpenWalls = Opening.All;
-                }
-                else
-                {
-                    currentRoom = rooms.ElementAt(cmpt);
-                    currentRoom.OpenWalls |=
-                        (this._r.Next(100) < VerticalChance ? Opening.Up : Opening.None) |
-                        (this._r.Next(100) < HorizontalChance ? Opening.Right : Opening.None) |
-                        (this._r.Next(100) < VerticalChance ? Opening.Down : Opening.None) |
-                        (this._r.Next(100) < HorizontalChance ? Opening.Left : Opening.None);
-                    cmpt += 1;
-                }
-
-                this.CreateAdjacentRoom(currentRoom, rooms, Opening.Up);
-                this.CreateAdjacentRoom(currentRoom, rooms, Opening.Right);
-                this.CreateAdjacentRoom(currentRoom, rooms, Opening.Down);
-                this.CreateAdjacentRoom(currentRoom, rooms, Opening.Left);
-            }
-
-            // TODO : retourner un modèle exploitable
+            components = new List<Component>();
+            blocks = new List<Block>();
         }
 
-        private void CreateAdjacentRoom(Room currentRoom, List<Room> rooms, Opening direction)
+        public void AddComponent(String filePath)
         {
-            if ((currentRoom.OpenWalls & direction) == direction)
+            Component c = JsonConvert.DeserializeObject<Component>(System.IO.File.ReadAllText(filePath));
+            components.Add(c);
+        }
+
+        public void AddBlock(String filePath)
+        {
+            Block b = JsonConvert.DeserializeObject<Block>(System.IO.File.ReadAllText(filePath));
+            blocks.Add(b);
+        }
+
+        public List<Block> GetBlocks()
+        {
+            return blocks;
+        }
+
+        public void AnalyseBlocks()
+        {
+            foreach (Block b in blocks)
             {
-                Opening oppositeDirection;
-                Point newPosition = new Point(currentRoom.Position.X, currentRoom.Position.Y);
-                switch (direction)
-                {
-                    case Opening.Up:
-                        newPosition.Y -= 1;
-                        oppositeDirection = Opening.Down;
-                        break;
-                    case Opening.Right:
-                        newPosition.X += 1;
-                        oppositeDirection = Opening.Left;
-                        break;
-                    case Opening.Down:
-                        newPosition.Y += 1;
-                        oppositeDirection = Opening.Up;
-                        break;
-                    case Opening.Left:
-                        newPosition.X -= 1;
-                        oppositeDirection = Opening.Right;
-                        break;
-                    default:
-                        throw new Exception(string.Format("Attention au paramètre direction. {0}", direction));
-                }
+                b.BindComponents(components);
+                AnalyseBlock(b);
+            }
+        }
 
-                Room oldRoom = rooms.FirstOrDefault(ro => ro.Position == newPosition);
-                if (oldRoom != null)
-                {
-                    if (this._r.Next(100) < JoinChance)
-                    {
-                        oldRoom.OpenWalls |= oppositeDirection;
-                    }
-                    else if ((oldRoom.OpenWalls & oppositeDirection) != oppositeDirection)
-                    {
-                        currentRoom.OpenWalls &= (Opening.All & ~direction);
-                    }
+        protected void AnalyseBlock(Block b)
+        {
+            foreach (BlockComponent c in BorderNonSolidComponents(b))
+            {
 
-                }
-                else
+            }
+        }
+
+        protected IEnumerable<BlockComponent> BorderComponents(Block b)
+        {
+            for (int i = 0; i < b.BlockSize; i++)
+            {
+                if (i == 0 || i == b.BlockSize)
                 {
-                    Room newRoom = new Room()
+                    foreach (BlockComponent c in b.ComponentMatrix[i])
                     {
-                        Position = newPosition,
-                        OpenWalls = oppositeDirection
-                    };
-                    rooms.Add(newRoom);
+                        yield return c;
+                    }
+                }
+                else {
+                    yield return b.ComponentMatrix[i][0];
+                    yield return b.ComponentMatrix[i][b.BlockSize - 1];
                 }
             }
         }
 
-        [Flags]
-        enum Opening
+        protected IEnumerable<BlockComponent> BorderNonSolidComponents(Block b)
         {
-            Up = 0x0001,
-            Right = 0x0010,
-            Down = 0x0100,
-            Left = 0x1000,
-            All = 0x1111,
-            None = 0x0000
+            foreach (BlockComponent c in BorderComponents(b))
+            {
+                if (!c.Component.Solid)
+                {
+                    yield return c;
+                }
+            }
         }
 
-        class Room
+        public byte[,] GenerateChunck(int width, int height)
         {
-            public Opening OpenWalls { get; set; }
-            public Point Position { get; set; }
+            // Le chunk est initialisé avec toutes les valeurs par défaut (0 -> aucune ouverture)
+            // x représente les lignes, y les colonnes
+            byte[,] result = new byte[height, width];
+
+            // Liste des coordonnées à traiter
+            Queue<Point> toTreat = new Queue<Point>();
+
+            // On commence avec la position en haut à gauche
+            toTreat.Enqueue(new Point(0, 0));
+
+            // Génération des pièces
+            bool endReached = false;
+            // Tant qu'on a des pièces à traiter ou qu'on n'a pas atteint la fin
+            while (toTreat.Count > 0 || !endReached)
+            {
+                Point currentCoord;
+                if (toTreat.Count > 0)
+                {
+                    currentCoord = toTreat.Dequeue();
+                }
+                else
+                {
+                    currentCoord = this.FindTopRightNotNull(result);
+                }
+
+                // analyse de la faisabilité du tirage et initialisation des case adjacentes
+                // 4 tests minimum
+                this.CheckOpening(result, currentCoord, Opening.Up, toTreat);
+                this.CheckOpening(result, currentCoord, Opening.Right, toTreat);
+                this.CheckOpening(result, currentCoord, Opening.Down, toTreat);
+                this.CheckOpening(result, currentCoord, Opening.Left, toTreat);
+
+                if (currentCoord.Y == width - 1)
+                    endReached = true;
+            }
+            return result;
         }
+
+        private void CheckOpening(byte[,] array, Point currentCoord, Opening direction, Queue<Point> toTreat)
+        {
+            Opening oppositeOpening;
+            bool isValidPosition;
+            Point adjacentCoord;
+            int creationRate;
+            switch (direction)
+            {
+                case Opening.Up:
+                    oppositeOpening = Opening.Down;
+                    isValidPosition = currentCoord.X > 0;
+                    adjacentCoord = new Point(currentCoord.X - 1, currentCoord.Y);
+                    creationRate = 25;
+                    break;
+                case Opening.Right:
+                    oppositeOpening = Opening.Left;
+                    isValidPosition = currentCoord.Y < array.GetLength(1) - 1;
+                    adjacentCoord = new Point(currentCoord.X, currentCoord.Y + 1);
+                    creationRate = 50;
+                    break;
+                case Opening.Down:
+                    oppositeOpening = Opening.Up;
+                    isValidPosition = currentCoord.X < array.GetLength(0) - 1;
+                    adjacentCoord = new Point(currentCoord.X + 1, currentCoord.Y);
+                    creationRate = 25;
+                    break;
+                case Opening.Left:
+                    oppositeOpening = Opening.Right;
+                    isValidPosition = currentCoord.Y > 0;
+                    adjacentCoord = new Point(currentCoord.X, currentCoord.Y - 1);
+                    creationRate = 50;
+                    break;
+                default:
+                    throw new Exception("Paramètre direction invalide");
+            }
+
+            // on peut créer une case dans cette direction
+            if (isValidPosition && this._r.Next(100) < creationRate)
+            {
+                if (array[adjacentCoord.X, adjacentCoord.Y] == (byte)Opening.None)
+                {
+                    toTreat.Enqueue(adjacentCoord);
+                }
+                array[adjacentCoord.X, adjacentCoord.Y] |= (byte)oppositeOpening;
+                array[currentCoord.X, currentCoord.Y] |= (byte)direction;
+            }
+        }
+
+        private Point FindTopRightNotNull(byte[,] array)
+        {
+            for (int column = array.GetLength(1) - 1; column >= 0; column--)
+            {
+                for (int line = array.GetLength(0) - 1; line >= 0; line--)
+                {
+                    if (array[line, column] != 0)
+                    {
+                        Point found = new Point(line, column);
+                        Console.WriteLine("Right : {0}", found);
+                        return found;
+                    }
+
+                }
+            }
+            return new Point(0, 0);
+        }
+    }
+
+    [Flags]
+    public enum Opening : byte
+    {
+        None = 0,
+        Up = 1,
+        Right = 2,
+        Down = 4,
+        Left = 8
     }
 }
 
