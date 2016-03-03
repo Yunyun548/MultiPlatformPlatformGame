de9321f060489b7eb8b241fa7c50fbccd12f4c8e
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2016-03-03 14:09:00 +0100

    Add tests components and blocks

--- TestGeneration/Program.cs
+++ TestGeneration/Program.cs
@@ -10,8 +10,13 @@ namespace TestGeneration
 		public static void Main (string[] args)
 		{
 			Generator g = new Generator ();
-			g.AddComponent (@"../../test-files/component.json");
-			g.AddBlock (@"../../test-files/block.json");
+			g.AddComponent (@"../../test-files/empty-component.json");
+			g.AddComponent (@"../../test-files/wall-component.json");
+			g.AddBlock (@"../../test-files/corridor-block.json");
+			g.AddBlock (@"../../test-files/well-block.json");
+			g.AddBlock (@"../../test-files/plus-block.json");
+			g.AddBlock (@"../../test-files/up-right-l-block.json");
+			g.AddBlock (@"../../test-files/down-right-l-block.json");
 
 			g.AnalyseBlocks ();
 			g.GenerateChunck ();
