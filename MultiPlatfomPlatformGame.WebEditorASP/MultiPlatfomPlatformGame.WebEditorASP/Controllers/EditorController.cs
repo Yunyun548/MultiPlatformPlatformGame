ddd5dbc84c9317b19b129cca454dff92ebb2f479
Narkolapin <adrien.marty@ynov.com>
2016-03-03 17:21:22 +0100

    Solution ASP 4.5

---
+++ MultiPlatfomPlatformGame.WebEditorASP/MultiPlatfomPlatformGame.WebEditorASP/Controllers/EditorController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace MultiPlatfomPlatformGame.WebEditorASP.Controllers
+{
+    public class EditorController : Controller
+    {
+
+        private MySqlConnection _connexion;
+        private String _connectionString = "SERVER=vps2.lewebfrancais.fr; DATABASE=webeditor; UID=webeditor; PASSWORD=QUxUx5WXDp6XHr9J";
+
+        // GET: Editor
+        public ActionResult Index()
+        {
+            IList<Models.Component> collection = new List<Models.Component>();
+
+            using(this._connexion = new MySqlConnection(this._connectionString))
+            {
+                this._connexion.Open();
+                String commandString = "SELECT * FROM component";
+
+                using (MySqlCommand command = new MySqlCommand(commandString, this._connexion))
+                {
+                    using(MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            collection.Add(new Models.Component(
+                                reader.GetInt32(0),
+                                reader.GetString(1),
+                                reader.GetString(2),
+                                reader.GetString(3)
+                            ));
+                        }
+                    }
+                }
+            }
+
+            return View(collection);
+        }
+
+        public JsonResult PersistJson()
+        {
+            return new JsonResult();
+        }
+    }
+}
---
+++ MultiPlatfomPlatformGame.WebEditorASP/MultiPlatfomPlatformGame.WebEditorASP/Models/Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiPlatfomPlatformGame.WebEditorASP.Models
+{
+    public class Component
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String TexturePath { get; set; }
+        public Properties Physics { get; set; }
+        public Component()
+        {
+
+        }
+
+        public Component(int id, string name, string texturePath, string physics)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.TexturePath = texturePath;
+            this.Physics = JsonConvert.DeserializeObject<Properties>(physics);
+        }
+    }
+}
